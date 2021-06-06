23c54496eb192a3a1744d99be0eece95503a1a09
aprihodko22 <tohathebest97@gmail.com>
2021-06-06 14:38:52 +0300

    1. Создать свое собственное исключение с названием AllWentWrong 2. создать метод. Метод должен делать следующие вещи. Рандомно выбирать цифру от 1 до 100. Если цифра больше 50, то выбрасывать исключение. Выброшенное исключение не нужно обрабатывать, а нужно пробросить с помощью throws 3. В методе мейн вызвать предыдущий метод и обработать исключение с помощью трай/кэтч. Если исключение было выбрашено, вывести его стактрейс. Если нет, написать что "все ок". В конце блока исключений(в finally) написать "трай кэтч блок заночен"

--- src/dzLesson10/Runner.java
+++ src/dzLesson10/Runner.java
@@ -1,11 +1,28 @@
 package dzLesson10;
 
 public class Runner {
-    public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.move();
-        dog.run();
+    public static void main(String[] args){
+//        Dog dog = new Dog();
+//        dog.move();
+//        dog.run();
+
+        try {
+            method();
+        } catch (AllWentWrong e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("трай кэтч блок заночен");
+        }
+
     }
 
-
+    public static void method() throws AllWentWrong{
+        double i = 1;
+        i = Math.random() *100;
+        if(i > 50){
+            throw  new AllWentWrong();
+        }else {
+            System.out.println("Cifra men'she 50");
+        }
+    }
 }
